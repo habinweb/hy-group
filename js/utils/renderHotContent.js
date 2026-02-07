f0428f5eccd2545fc9ed8a70fd804beeb1fa90ea
하빈 <habinkim0427@naver.com>
2026-02-07 17:58:56 +0900

    fix: 감상평 섹션 포스터 출력 레이아웃 이슈 해결

--- js/utils/renderHotContent.js
+++ js/utils/renderHotContent.js
@@ -1,3 +1,5 @@
+const MAX_STARS = 5;
+
 export function fillPosterCard(card, item) {
   const link = card.querySelector('[data-role="link"]');
   if (link) link.href = `sub.html?id=${item.id}`;
@@ -15,14 +17,26 @@ export function fillPosterCard(card, item) {
   if (date) date.textContent = item.date;
 
   const stars = card.querySelector('[data-role="stars"]');
-  if (stars) {
-    stars.innerHTML = "";
-    for (let i = 0; i < item.rating; i++) {
-      const li = document.createElement("li");
-      li.className = "score";
-      li.innerHTML = `<img src="img/star_49E99C.png" alt="star" />`;
-      stars.appendChild(li);
+  if (!stars) return;
+
+  stars.innerHTML = "";
+
+  for (let i = 0; i < MAX_STARS; i++) {
+    const li = document.createElement("li");
+    li.className = "score";
+
+    const img = document.createElement("img");
+
+    if (i < item.rating) {
+      img.src = "img/star_49E99C.png";
+      img.alt = "별점";
+    } else {
+      img.src = "img/star_6f6c76.png";
+      img.alt = "빈 별";
     }
+
+    li.appendChild(img);
+    stars.appendChild(li);
   }
 }
 
